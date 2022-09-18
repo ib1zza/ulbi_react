8f1a9972d16cb61d10daab63472404e1f85b9e0e
ib1zza <dremast1337@gmail.com>
2022-09-18 09:58:54 +0300

    1 step: added function component and class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Counter from './components/Counter';
+import ClassCounter from './components/ClassCounter';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [likes, setLikes] = useState(0);
+    const [value, setValue] = useState('string');
+
+    function increment() {
+        setLikes(likes + 1);
+    }
+    function decrement() {
+        setLikes(likes - 1);
+
+    }
+    return (
+        <div className="App">
+            likes = {likes};
+
+            <button onClick={increment}>increment</button>
+            <button onClick={decrement}>decrement</button>
+
+
+            <Counter></Counter>
+            <ClassCounter></ClassCounter>
+        </div >
+    );
 }
 
 export default App;
